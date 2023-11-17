4dc7ebf6be3c3de9b12f5358b547fc1b7c889136
HONEST-ZH <62385652+HONEST-ZH@users.noreply.github.com>
2023-11-18 00:43:41 +0800

    route_update

--- sever.py
+++ sever.py
@@ -5,9 +5,3 @@ sever = Flask(__name__)
 @sever.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-@sever.route("/bye")
-def goodbye_world():
-    return "<p>Goodbye, World!</p>"
-@sever.route("/fuxk")
-def goodbye_world():
-    return "<p>Fuxk you, World!</p>"

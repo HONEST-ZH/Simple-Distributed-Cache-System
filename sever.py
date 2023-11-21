b032d27fd32f336bd596552b184f99227d91f7f6
HONEST-ZH <62385652+HONEST-ZH@users.noreply.github.com>
2023-11-21 15:06:30 +0800

    update server

--- sever.py
+++ sever.py
@@ -1,7 +1,6 @@
 import flask
-from flask import Flask
-sever = Flask(__name__)
-
+from markupsafe import escape
+sever = flask.Flask(__name__)
 @sever.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"

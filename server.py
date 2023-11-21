1a10b4902197bc39713a29b5d0122790b210fa1c
HONEST-ZH <62385652+HONEST-ZH@users.noreply.github.com>
2023-11-21 17:59:41 +0800

    update server

---
+++ server.py
@@ -0,0 +1,30 @@
+import flask
+server = flask.Flask(__name__)
+cache = {}
+'''
+@server.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+'''
+@server.post("/server1/-d <kv>")
+def server_write(kv):
+    kv = flask.request.json(kv)
+    global cache
+    cache.update(kv)
+@server.get("/server1/<key>")
+def server_read(key):
+    if key in cache:
+        content = cache[key]
+        n_kv = {}
+        n_kv[key] = content
+        return n_kv, 200
+    else:
+        return None, 404
+@server.delete("/server1/<key>")
+def server_delete(key):
+    global cache
+    if key in cache:
+        del cache[key]
+        return 1,200
+    else:
+        return 0,200
--- sever.py
+++
@@ -1,6 +0,0 @@
-import flask
-from markupsafe import escape
-sever = flask.Flask(__name__)
-@sever.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"

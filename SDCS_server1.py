fc324b4352a8288ea6b6626fd25a7859fa6f6517
HONEST-ZH <62385652+HONEST-ZH@users.noreply.github.com>
2023-11-24 01:05:15 +0800

    update grpc

--- SDCS_server1.py
+++ SDCS_server1.py
@@ -4,7 +4,6 @@ from concurrent import futures
 import grpc
 import SDCS_pb2
 import SDCS_pb2_grpc
-
 server = flask.Flask(__name__)#实例化Flask服务器
 cache = {'hello':114,'bye':514}#预先为内存写入数据，便于检测
 
@@ -19,12 +18,12 @@ class SDCSServicer(SDCS_pb2_grpc.SDCSServicer):
     def finddata(self, request:SDCS_pb2.Key, unused_context
                  )->SDCS_pb2.Data:
         key = request.key
-        if key in cache:
+        if key in cache:#当前节点有
             value = cache[key]
             dict = {key,value}
             Data = SDCS_pb2.Data(data = str(dict))
             return Data
-        else:
+        else:#当前节点没有
             Data = SDCS_pb2.Data(data = '')
             return Data
     '''
@@ -34,28 +33,33 @@ class SDCSServicer(SDCS_pb2_grpc.SDCSServicer):
     '''
     def deletedata(self, request: SDCS_pb2.Key, unused_context
                  ) -> SDCS_pb2.State:
-        global cache
         key = request.key
-        if key in cache:
-            del cache[key]
-            State = SDCS_pb2.State(stete = 1)
+        if key in cache:#当前节点有
+            cache.pop(key)
+            State = SDCS_pb2.State(state = 1)
             return State
-        else:
-            State = SDCS_pb2.State(stete = 0)
+        else:#当前节点没有
+            State = SDCS_pb2.State(state = 0)
             return State
+    '''
+        响应其他的节点的写入请求
+        函数输入（请求）是SDCS_pb2中的Data类型，和未用到的 unused_context
+        函数输出（回复）是SDCS_pb2中的State类型
+    '''
+    def writedata(self, request: SDCS_pb2.Data, unused_context
+                   ) -> SDCS_pb2.State:
+        data = request.data
+        cache.update(data)
+        State = SDCS_pb2.State(state = 1)
+        return State
 ###开启rpc服务器###
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     SDCS_pb2_grpc.add_SDCSServicer_to_server(SDCSServicer(), server)
-    server.add_insecure_port("127.0.0.1:5000")
+    server.add_insecure_port("127.0.0.1:5000")#本节点的grpc服务器地址和端口号
     server.start()
     server.wait_for_termination()
-server()
-###rpc的客户端：从SDCS_pb2_grpc的SDCSStub中实例化一个stub。###
-channel1 = grpc.insecure_channel('127.0.0.1:5001')
-stub1 = SDCS_pb2_grpc.SDCSStub(channel1)
-channel2 = grpc.insecure_channel('127.0.0.1:5002')
-stub2 = SDCS_pb2_grpc.SDCSStub(channel2)
+
 #####################面向客户端的HTTP操作（基于flask）####################
 #默认显示
 @server.get("/")
@@ -78,48 +82,76 @@ def server_write():
         print("not json")
         return '-2'
     '''
-    global cache
-    #TODO：哈希判定写入哪个服务器
-    #TODO：向选好的服务器写入KV对
     data = request.get_json()
-    print(data)
-    cache.update(data)
-    return ''
-
+    for key in data:
+        res = hash(key)
+        node_num = res%3
+    if node_num == 0:
+        cache.update(data)
+        return ''
+    Data = SDCS_pb2.Data(data = data)
+    if node_num == 1:
+        stub[1].writedata(Data)
+    if node_num == 2:
+        stub[2].writedata(Data)
 #客户端从服务器获得数据
 @server.get("/<key>")
 def server_read(key):
+    n_kv = {}
     if key in cache:#本地查找
         content = cache[key]
-        n_kv = {}
         n_kv[key] = content
         return n_kv, 200
     else:#其他节点查找，调用rpc
         Key = SDCS_pb2.Key(key = key)
-        Data = stub.finddata(Key)
-        data = Data.data
-        if data != '':
-            n_kv = {}
-            n_kv[key] = data
-            return n_kv, 200
-        else:
-            return '', 404
+        for i in (0,3):
+            if i == selfnum:
+                continue
+            _stub = stub(i)
+            Data = _stub.finddata(Key)
+            data = Data.data
+            if data != '':#找到一个其他节点拥有key的数据
+                n_kv[key] = data
+                return n_kv, 200
+            else:#当前节点没有，找下一个
+                continue
+        return '', 404#都没有返回空
 #客户端从服务器删除数据
 @server.delete("/<key>")
 def server_delete(key):
-    global cache
-    if key in cache:
-        del cache[key]
+    if key in cache:#本地删除
+        cache.pop(key)
         return '1', 200
     else:
         Key = SDCS_pb2.Key(key=key)
-        State = stub.deletedata(Key)
-        state = State.state
-        if state:
-            return '1', 200
-        else:
-            return '0', 200
+        for i in (0, 3):
+            if i == selfnum:
+                continue
+            _stub = stub(i)
+            State = _stub.deletedata(Key)
+            state = State.state
+            if state:#找到一个删除了的节点
+                return '1', 200
+            else:#当前节点没有，找下一个
+                continue
+        return '0', 200#都没有返回0个删除
 #客户端从服务器获得所有数据目录
 @server.route("/all")
 def server_see_all():
     return cache
+
+if __name__ == "__main__":
+    #开启flask服务器
+    server.run(host ='127.0.0.1',port = '9527')
+    #开启grpc服务器
+    server()
+    ###rpc的客户端：从SDCS_pb2_grpc的SDCSStub中实例化一个stub。###
+    channel0 = grpc.insecure_channel('127.0.0.1:5001')#节点1存根
+    stub0 = SDCS_pb2_grpc.SDCSStub(channel0)
+    channel1 = grpc.insecure_channel('127.0.0.1:5001')#节点2存根
+    stub1 = SDCS_pb2_grpc.SDCSStub(channel1)
+    channel2 = grpc.insecure_channel('127.0.0.1:5002')#节点3存根
+    stub2 = SDCS_pb2_grpc.SDCSStub(channel2)
+    stub = [stub0, stub1, stub2]#存根列表
+    selfnum = 0#本节点的序号
+
